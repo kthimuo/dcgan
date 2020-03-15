b36c99804ed81c3edf9cbc61bc012a5e377f6eeb
tamura <tamura@gmail.com>
2020-03-15 08:29:33 +0000

    add apples

--- generate_sketch.py
+++ generate_sketch.py
@@ -2,20 +2,25 @@ import sys,os
 from PIL import Image    
 from dcgan import DCGAN
 import numpy as np
-batch_size = 128
+
+import tensorflow as tf
+print(tf.__version__)
+batch_size = 32
 n_noise = 100
 dcgan = DCGAN(batch_size, n_noise)
 #
 dataset = []
-dir_name = 'apples'
+dir_name = 'sketch'
 for name in os.listdir(dir_name):
     if not name.startswith('.'):
         path = dir_name + '/'  + name
         img = Image.open(path)
         img = img.resize((64,64))
         img = np.array(img,dtype=np.float32)
-        img = (img - 127.5) / 127.5
-        dataset.append(img)
+#        img = (img - 127.5) / 127.5
+        
+        if img.shape == (64, 64,3):
+            dataset.append(img)
 dataset = np.array(dataset)
-dcgan.train(dataset,10000)
+dcgan.train(dataset,1000)
 
